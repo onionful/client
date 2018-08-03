c291c99ce1a04b9a89fdbfa14be52b6571d47de6
hsz <jakub@chrzanowski.info>
2018-08-03 11:16:53 +0200

    use glamor/reset instead of normalize.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { ConnectedRouter } from 'connected-react-router/immutable';
 import { App } from 'containers';
-import 'normalize.css';
 import { render } from 'react-dom';
 import { LocalizeProvider } from 'react-localize-redux';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import createStore, { history } from 'store';
-import { React, css } from 'utils/create';
+import { css, React } from 'utils/create';
 
 import registerServiceWorker from './registerServiceWorker';
 
