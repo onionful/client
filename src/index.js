97ad26b8756b34668d9f480a0f351a74c0840f00
hsz <jakub@chrzanowski.info>
2018-09-16 12:52:23 +0200

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { ConnectedRouter } from 'connected-react-router/immutable';
 import App from 'containers/App';
 import 'glamor/reset';
@@ -7,25 +6,18 @@ import { LocalizeProvider } from 'react-localize-redux';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import createStore, { history } from 'store';
-import { css, React } from 'utils/create';
 import Typography from 'typography';
 import deYoungTheme from 'typography-theme-de-young';
-
-import { fonts } from 'utils/variables';
+import { css, React } from 'utils/create';
 
 import registerServiceWorker from './registerServiceWorker';
 
 const typography = new Typography(deYoungTheme);
-
-// css.global('#root', { height: '100%', fontFamily: fonts.roboto });
-// css.global('h1, h2, h3, h4, h5, h6', { fontFamily: fonts.rokkitt });
-// css.insert(fonts.url);
-//
-css.global('#root', { height: '100%' });
-css.insert(typography.toString());
-
 const store = createStore();
 const root = document.getElementById('root');
+
+css.global('#root', { height: '100%' });
+css.insert(typography.toString());
 
 (root.hasChildNodes() ? hydrate : render)(
   <Provider store={store}>
