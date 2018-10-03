7d35516b20e0932ba9683b45ad8d94b8018cac95
hsz <jakub@chrzanowski.info>
2018-10-03 13:27:58 +0200

    enhanced Markdown component

--- src/components/Markdown/index.js
+++ src/components/Markdown/index.js
@@ -1,12 +1,42 @@
-import { PropTypes, React } from 'utils/create';
+import Gallery from 'components/Gallery';
 import ReactMarkdown from 'react-markdown';
+import RemarkShortcodes from 'remark-shortcodes';
+import { PropTypes, React } from 'utils/create';
 
-const Markdown = ({ source, ...props }) => (
-  <ReactMarkdown source={source} escapeHtml={false} {...props} />
+const Markdown = ({ source, images = {}, ...props }) => (
+  <ReactMarkdown
+    {...props}
+    source={source}
+    escapeHtml={false}
+    plugins={[[RemarkShortcodes, { startBlock: '[[', endBlock: ']]' }]]}
+    renderers={{
+      shortcode: ({ identifier, attributes }) => {
+        switch (identifier) {
+          case 'Gallery':
+            return (
+              <Gallery
+                images={(attributes.ids || '').split(',').map(id => ({
+                  ...images[id],
+                  original: images[id].url,
+                }))}
+              />
+            );
+
+          default:
+            return '';
+        }
+      },
+    }}
+  />
 );
 
 Markdown.propTypes = {
   source: PropTypes.string.isRequired,
+  images: PropTypes.object, // eslint-disable-line
+};
+
+Markdown.defaultProps = {
+  images: {},
 };
 
 export default Markdown;
